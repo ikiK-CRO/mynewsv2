d378945969e8a894b66e00a1cef144c3e5e0b221
ikiK <kiki.ikik@gmail.com>
2025-04-08 14:32:59 +0200

    first commit

---
+++ src/app/signin/page.js
@@ -0,0 +1,13 @@
+export default function SignIn() {
+  return (
+    <main>
+      <h1>Sign In</h1>
+      <div>
+        <p>This is a clean sign in page.</p>
+        <div>
+          <a href="/">Back to Home</a>
+        </div>
+      </div>
+    </main>
+  );
+} 
---
+++ src/app/signup/page.js
@@ -0,0 +1,13 @@
+export default function SignUp() {
+  return (
+    <main>
+      <h1>Sign Up</h1>
+      <div>
+        <p>This is a clean sign up page.</p>
+        <div>
+          <a href="/">Back to Home</a>
+        </div>
+      </div>
+    </main>
+  );
+} 
