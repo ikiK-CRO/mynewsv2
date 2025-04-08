a427ea953f1f374a2ca9ee4b7d06de2ea440e6be
ikiK <kiki.ikik@gmail.com>
2025-04-08 16:34:13 +0200

    top bar 99% finished, smal styling difrences

--- src/app/components/Topbar.js
+++ src/app/components/Topbar.js
@@ -5,7 +5,12 @@ export default function Topbar() {
     <header className={styles.topbar}>
       <div className="container">
         <div className={styles.topbarContent}>
-          {/* Topbar content will go here */}
+          <h1 className={styles.title}>Make MyNews your homepage</h1>
+          <p className={styles.subtitle}>Every day discover what's trending on the internet!</p>
+          <div className={styles.rightContent}>
+            <a href="#" className={styles.noThanks}>No, thanks</a>
+            <button className={styles.getButton}>GET</button>
+          </div>
         </div>
       </div>
     </header>
