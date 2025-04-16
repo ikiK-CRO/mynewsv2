d301f9934ac27c011ca7b4f3c5b513d540f6a440
ikiK <kiki.ikik@gmail.com>
2025-04-16 04:53:09 +0200

    mobile menu fixes

--- src/app/components/MobileMenu.tsx
+++ src/app/components/MobileMenu.tsx
@@ -66,18 +66,21 @@ const MobileMenu: React.FC<MobileMenuProps> = ({
 
   return (
     <div className={styles.mobileMenuWrapper}>
-      <div className={styles.mobileMenuHeader}>
-        <a href="/" className={styles.logoLink} onClick={handleLogoClick}>
-          <div className={styles.logo}>
-            <span>My</span>News
-          </div>
-        </a>
+      <div className={styles.closeButtonWrapper}>
         <button className={styles.closeButton} onClick={onClose}>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18 6L6 18" stroke="#1D1D1B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M6 6L18 18" stroke="#1D1D1B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         </button>
+      </div>
+      
+      <div className={styles.logoWrapper}>
+        <a href="/" className={styles.logoLink} onClick={handleLogoClick}>
+          <div className={styles.logo}>
+            <span>My</span>News
+          </div>
+        </a>
       </div>
       
       <form className={styles.searchForm} onSubmit={handleSearch}>
